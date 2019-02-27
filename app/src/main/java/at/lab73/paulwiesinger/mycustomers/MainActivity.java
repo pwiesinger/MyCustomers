cc6a047c66e400e2b0464033857380fccbffe5c0
pwiesinger <paul.wiesinger@lab73.at>
2019-02-28 00:36:11 +0100

    first version of the setup function finished

--- app/src/main/java/at/lab73/paulwiesinger/mycustomers/MainActivity.java
+++ app/src/main/java/at/lab73/paulwiesinger/mycustomers/MainActivity.java
@@ -2,12 +2,39 @@ package at.lab73.paulwiesinger.mycustomers;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.SearchView;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ListView listView;
+    private SearchView searchView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        listView = findViewById(R.id.listView);
+        searchView = findViewById(R.id.searchView);
+
+        setupData();
+    }
+
+
+    private void setupData() {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(getAssets().open("customers_data.csv")))) {
+            String l = br.readLine();
+            while(l != null) {
+                System.out.println(l);
+                l = br.readLine();
+            }
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
     }
 }
