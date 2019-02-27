31232a66dbc653bdf345ff47c354aecd78850ba5
pwiesinger <paul.wiesinger@lab73.at>
2019-02-28 00:41:19 +0100

    added customer.java

---
+++ app/src/main/java/at/lab73/paulwiesinger/mycustomers/Customer.java
@@ -0,0 +1,38 @@
+package at.lab73.paulwiesinger.mycustomers;
+
+public class Customer {
+
+    private int id;
+    private String firstname;
+    private String lastname;
+
+    public Customer(int id, String firstname, String lastname) {
+        this.id = id;
+        this.firstname = firstname;
+        this.lastname = lastname;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+}
